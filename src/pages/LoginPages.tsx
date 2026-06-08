4d448c20560c7ada76bc631ab5b48eee34b8f2a7
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-07 21:34:27 -0300

    style: Mudando forma do login

--- src/pages/LoginPages.tsx
+++ src/pages/LoginPages.tsx
@@ -4,6 +4,7 @@ export default function LoginPages() {
     return (
         <>
             <div className={styles.card}>
+                <h1 className="head">Logo</h1>
                 <h1 className="head">Libary Fast</h1>
                 <div className={styles.inputs}>
                     <label>
@@ -14,9 +15,13 @@ export default function LoginPages() {
                         Senha:
                         <input type="password" name="user" />
                     </label>
+                    <p>Esqueci minha senha</p>
                 </div>
                 <button className={styles.btn}>Entrar</button>
+                <p className={styles.forgotPassword}>Ainda não tenho uma conta</p>
             </div>
+
+            <p className={styles.byMe}>2026 | Desenvolvido por <a target='_blank' href='https://github.com/cauealb'>Cauê Alves</a></p>
         </>
     )
 }
