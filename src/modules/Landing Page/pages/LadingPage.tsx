d0975f02d02aee2a883003ad89d1db20e14545eb
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-09 20:56:30 -0300

    feat: Incluindo link na landing page

--- src/modules/Landing Page/pages/LadingPage.tsx
+++ src/modules/Landing Page/pages/LadingPage.tsx
@@ -2,15 +2,9 @@ import { IoPlayCircle } from 'react-icons/io5'
 import logo from '../../../assets/logo.png'
 import { ButtonCta } from '../components/ButtonCta/ButtonCta'
 import style from './style.module.css'
-import { Link, useNavigate } from 'react-router-dom'
-
-const navigate = useNavigate()
+import { Link } from 'react-router-dom'
 
 export default function LadingPage() {
-    function onClickGetStarted() {
-        navigate('/login')
-    }
-
     return (
         <>
             <header className={style.hero}>
@@ -19,18 +13,16 @@ export default function LadingPage() {
                 </div>
                 
                 <div className={style.opt}>
-                    <a href="">About us</a>
+                    <a href="">About us</a> 
                     <a href="">Pricing</a>
                     <a href="">Solutions</a>
                     <a href="">Features</a>
                     <a href="">Company</a>
                 </div>
 
-                <div onClick={onClickGetStarted}>
-                    <Link to="/">
-                        <ButtonCta title='Get Started'  />
-                    </Link>
-                </div>
+                <Link to="/login">
+                    <ButtonCta title="Get Started" />
+                </Link>
             </header>
             <div className={style.divMain}>
                 <h1 className={style.main}>Enhance your book organization with SaaS models</h1>
