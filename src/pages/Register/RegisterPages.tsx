c34f5fca92e6ebd1e84be573789ff8df8ef98152
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-10 01:14:39 -0300

    feat: Finalizando fontro de auth

--- src/pages/Register/RegisterPages.tsx
+++ src/pages/Register/RegisterPages.tsx
@@ -1,12 +1,13 @@
 import ButtonJoin from "../../components/ButtonJoin";
 import logo from '../../assets/logo.png'
 import styles from './style.module.css'
+import { Link } from "react-router-dom";
 
 export default function RegisterPages() {
     return (
         <>
-            <div>
-                <div>
+            <div className={styles.container}>
+                <div className={styles.registerContainer}>
                     <img className={styles.logo} src={logo} alt="Logo Libary Fast" />
                     <h1 className="head">Libary Fast</h1>
                     <div className={styles.inputs}>
@@ -18,11 +19,19 @@ export default function RegisterPages() {
                             Senha:
                             <input type="password" placeholder="*******" name="user" />
                         </label>
-                        <p className={styles.forgotPassword}>Esqueci minha senha</p>
                     </div>
-                    <ButtonJoin title="Entrar" />
-                    <p className={styles.noAccont}>Ainda não tenho uma conta</p>
+                    <ButtonJoin title="Criar conta" />
+                    <Link to="/login">
+                        <p className={styles.haveAccont}>Já tenho uma conta cadastrada</p>
+                    </Link>
                 </div>
+
+                <p className={styles.byMe}>
+                    2026 | Desenvolvido por{" "}
+                    <a target="_blank" href="https://github.com/cauealb">
+                    Cauê Alves
+                    </a>
+                </p>
             </div>
         </>
     )
