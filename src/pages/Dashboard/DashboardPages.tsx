72f5778af6400b8a742ad7e3320027bf2fea2476
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-11 22:27:17 -0300

    Incluindo componente de sidebar

--- src/pages/Dashboard/DashboardPages.tsx
+++ src/pages/Dashboard/DashboardPages.tsx
@@ -1,71 +1,20 @@
 import { useState } from 'react';
 import styles from './style.module.css'
-import { FaUser } from "react-icons/fa";
-import { IoIosExit } from "react-icons/io";
-import { FaBook } from "react-icons/fa";
-import { FaSwatchbook } from "react-icons/fa6";
-import { IoSettingsSharp } from "react-icons/io5";
-import { MdMenu } from "react-icons/md";
 import ModalYesOrNo from '../../components/ModalYesOrNo';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../Sidebar/Sidebar';
 
 export default function DashboardPages() {
     const [modal, setModal] = useState<boolean>(false)
-    const [expandMenu, setExpandMenu] = useState<boolean>(false)
-
-    function handleModalOpen() {
-        setModal(true)
-    }
 
     function handleModalClose() {
         setModal(false)
     }
 
-    function handleClickMenu() {
-        setExpandMenu(prev => prev ? false : true)
-    }
-
     return(
         <>
             <div className={styles.container}>
-                <aside className={`${styles.sidebar} ${expandMenu ? styles.sidebarCollapsed : ''}`}>
-                    <div className={styles.navSidebar}>
-                        <div className={styles.menu}>
-                            <MdMenu onClick={handleClickMenu} size={30} />
-                        </div>
-                        <div className={styles.contentuser}>
-                            <FaUser />
-                            <h2>Olá, Cauê</h2>
-                        </div>
-
-                        <nav className={styles.options}>
-                            <Link to="/dashboard/best-books">
-                                <div>
-                                    <FaBook />
-                                    <p>Livros</p>
-                                </div>
-                            </Link>
-                            <Link to="/dashboard/my-bookcase">
-                                <div>
-                                    <FaSwatchbook />
-                                    <p>Meus livros</p>
-                                </div>
-                            </Link>
-                            <Link to="/dashboard/config">
-                                <div>
-                                    <IoSettingsSharp />
-                                    <p>Configurações</p>
-                                </div>
-                            </Link>
-                        </nav>
-
-                    </div>
-                    
-                    <div onClick={handleModalOpen} className={styles.exit}>
-                        <IoIosExit size={28} />
-                        <h3>Sair</h3>
-                    </div>
-                </aside>
+                <Sidebar />
 
                 <main className={styles.outlet}>
                     <Outlet />
---
+++ src/pages/Sidebar/Sidebar.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import styles from './style.module.css'
+import { MdMenu } from 'react-icons/md'
+import { FaBook, FaSwatchbook, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { IoSettingsSharp } from 'react-icons/io5'
+import { IoIosExit } from 'react-icons/io'
+
+export default function Sidebar() {
+    const [modal, setModal] = useState<boolean>(false)
+    const [expandMenu, setExpandMenu] = useState<boolean>(true)
+
+    function handleModalOpen() {
+        setModal(true)
+    }
+
+    function handleClickMenu() {
+        setExpandMenu(prev => prev ? false : true)
+    }
+
+    return (
+        <>
+            <div className={styles.container}>
+                <aside className={`${styles.sidebar} ${expandMenu ? styles.sidebarCollapsed : ''}`}>
+                    <div className={styles.navSidebar}>
+                        <div className={styles.menu}>
+                            <MdMenu onClick={handleClickMenu} size={30} />
+                        </div>
+                        <div className={styles.contentuser}>
+                            <FaUser />
+                            <h2>Olá, Cauê</h2>
+                        </div>
+
+                        <nav className={styles.options}>
+                            <Link to="/dashboard/best-books">
+                                <div>
+                                    <FaBook />
+                                    <p>Livros</p>
+                                </div>
+                            </Link>
+                            <Link to="/dashboard/my-bookcase">
+                                <div>
+                                    <FaSwatchbook />
+                                    <p>Meus livros</p>
+                                </div>
+                            </Link>
+                            <Link to="/dashboard/config">
+                                <div>
+                                    <IoSettingsSharp />
+                                    <p>Configurações</p>
+                                </div>
+                            </Link>
+                        </nav>
+
+                    </div>
+                    
+                    <div onClick={handleModalOpen} className={styles.exit}>
+                        <IoIosExit size={28} />
+                        <h3>Sair</h3>
+                    </div>
+                </aside>
+            </div>
+        </>
+    )
+}
