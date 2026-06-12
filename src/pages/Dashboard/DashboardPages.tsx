b325343d66857b76f99b377877241b6d2e84cda2
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-11 22:40:30 -0300

    .

--- src/pages/Dashboard/DashboardPages.tsx
+++ src/pages/Dashboard/DashboardPages.tsx
@@ -1,16 +1,8 @@
-import { useState } from 'react';
 import styles from './style.module.css'
-import ModalYesOrNo from '../../components/ModalYesOrNo';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
 
 export default function DashboardPages() {
-    const [modal, setModal] = useState<boolean>(false)
-
-    function handleModalClose() {
-        setModal(false)
-    }
-
     return(
         <>
             <div className={styles.container}>
@@ -20,13 +12,6 @@ export default function DashboardPages() {
                     <Outlet />
                 </main>
             </div>
-
-            <ModalYesOrNo 
-                header='Deseja Sair?' 
-                question='Ao selecionar em sair, seu sistema será fechado e só retornará quando fazer seu login novamente.' 
-                modalOpen={modal}
-                isClose={handleModalClose} 
-            />
         </>
     )
 }
--- src/pages/Sidebar/Sidebar.tsx
+++ src/pages/Sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ import { FaBook, FaSwatchbook, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { IoSettingsSharp } from 'react-icons/io5'
 import { IoIosExit } from 'react-icons/io'
+import ModalYesOrNo from '../../components/ModalYesOrNo'
 
 export default function Sidebar() {
     const [modal, setModal] = useState<boolean>(false)
@@ -13,6 +14,11 @@ export default function Sidebar() {
     function handleModalOpen() {
         setModal(true)
     }
+
+    function handleModalClose() {
+        setModal(false)
+    }
+
 
     function handleClickMenu() {
         setExpandMenu(prev => prev ? false : true)
@@ -60,6 +66,13 @@ export default function Sidebar() {
                     </div>
                 </aside>
             </div>
+
+            <ModalYesOrNo 
+                header='Deseja Sair?' 
+                question='Ao selecionar em sair, seu sistema será fechado e só retornará quando fazer seu login novamente.' 
+                modalOpen={modal}
+                isClose={handleModalClose} 
+            />
         </>
     )
 }
