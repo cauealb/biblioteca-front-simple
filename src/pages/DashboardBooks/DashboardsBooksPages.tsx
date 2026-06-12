746d981949594b726f1c056251858a7dd955b8b9
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-12 00:38:14 -0300

    feat: finalizando estilo outlet

--- src/pages/DashboardBooks/DashboardsBooksPages.tsx
+++ src/pages/DashboardBooks/DashboardsBooksPages.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react'
 import style from './style.module.css'
 
-let arr = [
+interface APIProps {
+    top: number,
+    name: string,
+    qtd: number
+}
+
+let arr: APIProps[] = [
     {top: 1, name: "A Cabeça do Santo", qtd: 18},
     {top: 2, name: "É Assim que Acaba", qtd: 18},
     {top: 3, name: "O Lado Feio do Amor", qtd: 18},
@@ -32,12 +39,21 @@ let arr = [
 ]
 
 export default function DashboardsBooksPages() {
+    const [textBook, setTextBook] = useState<string>('');
+    const [listBookSearch, setListBookSearch] = useState<APIProps[]>([])
+
+    function handleTextChangedSearchBook(value: string) {
+        setTextBook(value)
+
+        setListBookSearch(arr)
+    }
+
     return (
         <>
             <div className={style.container}>
                 <div className={style.input}>
                     <h3>Pesquisar livro</h3>
-                    <input type="text" />
+                    <input type="text" onChange={(e) => handleTextChangedSearchBook(e.target.value)} />
                 </div>
 
                 <div className={style.table}>
@@ -51,7 +67,7 @@ export default function DashboardsBooksPages() {
                         </thead>
 
                         <tbody>
-                            {arr.map(book => (
+                            {listBookSearch.map(book => (
                                 <tr key={book.top}>
                                     <td>{book.top}</td>
                                     <td>{book.name}</td>
