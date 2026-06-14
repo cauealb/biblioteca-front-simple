d5c102a2f067f1912d5fb97370e3b908a89c1eaf
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-14 14:57:47 -0300

    feat: Incluindo a Hom

--- src/pages/Navbar/Navbar.tsx
+++ src/pages/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from './style.module.css'
-import { FaBook, FaSwatchbook } from "react-icons/fa";
+import { FaBook, FaHome, FaSwatchbook } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
 
 interface NavbarProps {
@@ -11,22 +11,28 @@ export default function Navbar({ expandMenu }: NavbarProps) {
     return (
         <>
             <nav className={`${styles.options} ${expandMenu ? styles.navbarCollapsed : ''}`}>
+                <Link to="/dashboard">
+                    <div>
+                        <FaHome />
+                        <p>Home</p>
+                    </div>
+                </Link>
                 <Link to="/dashboard/best-books">
                     <div>
                         <FaBook  />
-                        <p>Livros</p>
+                        <p>Book</p>
                     </div>
                 </Link>
                 <Link to="/dashboard/my-bookcase">
                     <div>
                         <FaSwatchbook  />
-                        <p>Meus livros</p>
+                        <p>My Books</p>
                     </div>
                 </Link>
                 <Link to="/dashboard/config">
                     <div>
                         <IoSettingsSharp  />
-                        <p>Configurações</p>
+                        <p>Settings</p>
                     </div>
                 </Link>
             </nav>
