6fbe5c8d76b704a42291b7e9d03d981786182434
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-14 00:28:20 -0300

    Ajustando card

--- src/components/CardBook.tsx
+++ src/components/CardBook.tsx
@@ -1,19 +1,19 @@
 import { FaCircle } from "react-icons/fa";
 import style from  './style.module.css'
 
-interface CardBookProps {
+export interface CardBookProps {
     idBook: number,
     title: string,
     read: boolean
 }
 
-export default function CardBook({ idBook, title, read }: CardBookProps) {
+export default function CardBook({ title, read }: CardBookProps) {
     return (
         <>
             <div className={style.cardBook}>
                 <h3>Nome: {title}</h3>
                 <div>
-                    {read ? <FaCircle color={`${read ? '#ff5454' : '#82ff54}'`} />'}
+                    <FaCircle color={`${read ? 'green' : 'red'}`} />
                     <button>Detalhes</button>
                 </div>
             </div>
--- src/pages/MyBookcase/MyBookcase.tsx
+++ src/pages/MyBookcase/MyBookcase.tsx
@@ -1,5 +1,84 @@
-import CardBook from '../../components/CardBook'
+import CardBook, { type CardBookProps } from '../../components/CardBook'
 import style from './style.module.css'
+
+
+let books:CardBookProps[] = [
+    {
+        idBook: 1,
+        title: "Harry Poter",
+        read: true
+    },
+    {
+        idBook: 2,
+        title: "As Crônicas de Nârnia",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    },
+    {
+        idBook: 3,
+        title: "Meu nome é Êneias.",
+        read: true
+    }
+]
 
 export default function MyBookcase() {
     return (
@@ -7,7 +86,16 @@ export default function MyBookcase() {
             <div className={style.container}>
                 <h1>Minha lista</h1>
 
-                <CardBook idBook={1} title={"Harry Potter"} read={false} />
+                <div className={style.list}>
+                    {books.map(book => (
+                        <CardBook 
+                            key={book.idBook}
+                            idBook={book.idBook}
+                            title={book.title}
+                            read={book.read}
+                        />
+                    ))}
+                </div>
             </div>
         </>
     )
