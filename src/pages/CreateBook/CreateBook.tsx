ab5ac3ea57101886064750c2611cda7a4e1a9d38
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-15 23:05:14 -0300

    feat: Incluindo formulário

--- src/pages/CreateBook/CreateBook.tsx
+++ src/pages/CreateBook/CreateBook.tsx
@@ -10,36 +10,57 @@ export default function CreateBook() {
                     <p>Simple and eazy create your book :)</p>
                 </header>
 
-                <div>
-                    <label>
-                        Book name:
-                        <input type="text" />
-                    </label>
+                <div className={style.containerInputs}>
+                    <div className={`${style.inputs} ${style.panel}`}>
+                        <label>
+                            Book name:
+                            <input type="text" placeholder='Harry Potter' />
+                        </label>
 
-                    <label>
-                        Author name:
-                        <input type="text" />
-                    </label>
+                        <label>
+                            Author name:
+                            <input type="text" placeholder='J. K. Rowling'  />
+                        </label>
 
-                    <label>
-                        Published:
-                        <input type="date" />
-                    </label>
+                        <label>
+                            Published:
+                            <input type="date" />
+                        </label>
+                        
+                        <label>
+                            Status
+                            <select>
+                                <option>Lendo</option>
+                                <option>Lido</option>
+                                <option>Pendente</option>
+                            </select>
+                        </label>
 
-                    <label>
-                        Published:
-                        <select>
-                            <option>Programação</option>
-                            <option>Auto ajuda</option>
-                            <option>Romance</option>
-                            <option>Ficção Científica</option>
-                            <option>Terror</option>
-                            <option>Estudo</option>
-                        </select>
-                    </label>
+                        <label>
+                            Published:
+                            <select>
+                                <option>Programação</option>
+                                <option>Auto ajuda</option>
+                                <option>Romance</option>
+                                <option>Ficção Científica</option>
+                                <option>Terror</option>
+                                <option>Estudo</option>
+                            </select>
+                        </label>
+                        <footer className={style.footerBtn}>
+                            <button className={style.clear}>
+                                Limpar
+                            </button>
+                            <PrimaryAction title='Salvar livro'/>
+                        </footer>
+                    </div>
+
+                    <div className={`${style.listBooks} ${style.panel}`}>
+                        <h4>Livros criados</h4>
+                    </div>
                 </div>
+                
 
-                <PrimaryAction title='Salvar'/>
             </div>
         </>
     )
